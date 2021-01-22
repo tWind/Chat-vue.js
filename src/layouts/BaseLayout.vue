24a1f3403962efe2c3dd47087ffb9dfb80baac19
tWind <su.ilveran@gmail.com>
2021-01-22 11:53:11 +0300

    start project - base layout

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="base-layout">
+    <div class="layout-container">
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'BaseLayout',
+  };
+</script>
+
+<style lang="scss" scoped>
+.layout-container {
+  display: flex;
+}
+</style>
---
+++ src/views/ChatView.vue
@@ -0,0 +1,16 @@
+<template>
+  <base-layout>
+    <h2>chat here</h2>
+  </base-layout>
+</template>
+
+<script>
+import BaseLayout from '@/layouts/BaseLayout';
+
+export default {
+  name: 'ChatView',
+  components: {
+    BaseLayout,
+  },
+}
+</script>
